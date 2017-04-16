a3f0a48739bc1c097e97725c9faf09e65e0b932d
Daisuke <siq009@yahoo.ca>
2017-04-16 13:36:20 -0400

    Version 2.0 - Rough Design has been done and worked witout errors.

--- app/src/main/java/jp/techacademy/katahara/daisuke/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/katahara/daisuke/autoslideshowapp/MainActivity.java
@@ -11,8 +11,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.net.Uri;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ImageView;
 import android.os.Handler;
 
 import java.util.Timer;
@@ -25,11 +27,12 @@ public class MainActivity extends AppCompatActivity {
     Button mnextButton;
     Button mprevButton;
     Button mplayButton;
-    TextView mimageView;
+    TextView mtextView2;
 
     Timer mTimer;
-
     Handler mHandler = new Handler();
+
+    Boolean autoplay = false; // スライドショー機能が選択されているかどうかのチェック
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,24 +70,136 @@ public class MainActivity extends AppCompatActivity {
 
         // 画像の情報を取得する
         ContentResolver resolver = getContentResolver();
-        Cursor cursor = resolver.query(
+        final Cursor cursor = resolver.query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
                 null, // 項目(null = 全項目)
                 null, // フィルタ条件(null = フィルタなし)
                 null, // フィルタ用パラメータ
                 null  // ソート (null = ソートなし)
-        ):
+        );
+
+        mtextView2 = (TextView) findViewById(R.id.textView2);
 
         if (cursor.moveToFirst()) {
-            do {
-                // indexからIDを取得し、そのIDから画像のURIを取得する
+
+                // ボタンの初期化
+                mnextButton = (Button) findViewById(R.id.nextButton);
+                mprevButton = (Button) findViewById(R.id.prevButton);
+                mplayButton = (Button) findViewById(R.id.playButton);
+
+                // 一番最初の画像を表示（初期表示）
                 int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
                 Long id = cursor.getLong(fieldIndex);
                 Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
-                Log.d("ANDROID", "URI:" + imageUri.toString());
-            } while (cursor.moveToNext());
+                ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+                imageVIew.setImageURI(imageUri);
+
+        } else {
+          // 画像データが存在しない場合
+          mtextView2.setText("画像データが1枚も存在しません。");
+          cursor.close();
         }
-        cursor.close();
+
+        // "進む"ボタンが押された際の処理
+        mnextButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (autoplay) {
+                    // スライドショー起動時は処理なし
+                } else {
+
+
+                    // 画像ファイルの一番最後まで来ていた場合は、一番最初に戻る
+                    if (cursor.moveToNext() == false) {
+                        cursor.moveToFirst();
+                    }
+
+                    // 次の画像の表示
+                    int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                    Long id = cursor.getLong(fieldIndex);
+                    Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                    ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+                    imageVIew.setImageURI(imageUri);
+                }
+            }
+        });
+
+        // "戻る"ボタンが押された際の処理
+        mprevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (autoplay) {
+                // スライドショー起動時は処理なし
+                } else {
+
+                // 画像ファイルの一番最初まで来ていた場合は、一番最後に戻る
+                if (cursor.moveToPrevious() == false) {
+                    cursor.moveToLast();
+                }
+
+                // 次の画像の表示
+                int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                Long id = cursor.getLong(fieldIndex);
+                Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+                imageVIew.setImageURI(imageUri);
+                }
+            }
+        });
+
+        // "再生 / 停止"ボタンが押された際の処理
+        mplayButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (autoplay) {
+                    autoplay = false;
+                    mTimer.cancel();
+                } else {
+
+                    Log.d("DEBUG", "POINT 01");
+
+                    // スライドショーの開始
+                    autoplay = true;
+                    mTimer = new Timer();
+                    mTimer.schedule(new TimerTask() {
+
+                        @Override
+                        public void run() {
+
+                            // 画像ファイルの一番最後まで来ていた場合は、一番最初に戻る
+                            if (cursor.moveToNext() == false) {
+                                cursor.moveToFirst();
+                            }
+
+                            mHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    // indexからIDを取得し、そのIDから画像のURIを取得する
+                                    int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                                    Long id = cursor.getLong(fieldIndex);
+                                    Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                                    ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+                                    imageVIew.setImageURI(imageUri);
+
+                                    Log.d("ANDROID", "URI:" + imageUri.toString()); // 確認のためにログ出力しておく
+                                }
+                            });
+                        }
+                    }, 0, 2000);
+
+                }
+
+            }
+        });
+
     }
 }
